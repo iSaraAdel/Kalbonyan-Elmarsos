b30bfeef67a005c372a7f5dc7b6301d53c594d47
Sara Adel <83007568+iSaraAdel@users.noreply.github.com>
2022-04-25 00:32:29 +0200

    Update Guessing-Game-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Guessing-Game-Challenge.py
@@ -1,1 +1,12 @@
+# guess favorit food
 
+favorit_food="banana"
+
+anwser = input("What is my favorit food?")
+
+if anwser==favorit_food:
+    print("Yep! So amazing!") 
+else:
+    print("Yuck! That's not it") 
+   
+print("Thanks for playing!")
