bb416e6283142ac39c0f3e85380e4e688cf682df
Sara Adel <83007568+iSaraAdel@users.noreply.github.com>
2022-04-25 00:35:20 +0200

    Update Working-with-collections-challenge.py

--- Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ Linkedin-Learning/Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,43 @@
+# Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
 
+#create a list 
+
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+
+#refrence list data
+
+print(stars[3])
+
+# Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+# creat a dictionary
+
+peaks = {
+     'African' :'Kilimanjaro',
+    'Antarctic' : 'Vinson',
+    'Australian' : 'Puncak Jaya',
+    'Eurasian' :'Everest',
+    'North_American' : 'Denali',
+    'Pacific' : 'Mauna Kea',
+    'South_American' : 'Aconcagua',   
+}
+
+#refrence dictionary item 
+
+print(peaks[ 'Pacific'])
