1ba80d189359b2f7a3cbbe4b7fb503a732ff2f01
Sara Adel <83007568+iSaraAdel@users.noreply.github.com>
2022-04-28 22:54:03 +0200

    Update Favorite-Cities-Challenge.py

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -1,1 +1,8 @@
-
+# Print out the name of a favorite city 
+def favorite_city(name):
+    print('One of may favorite cities is', name)
+    
+favorite_city('Santa Barbara, California')
+favorite_city('Asheville, North Carolina')
+favorite_city('Amsterdam, The NetherLands')
+ 
